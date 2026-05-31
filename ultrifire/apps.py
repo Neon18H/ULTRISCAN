df0ce4eac315f8882bdbfa7c6cbf5658f2bdf7b6
Fabio Hams Valois <92942417+Neon18H@users.noreply.github.com>
2026-05-30 23:35:48 -0500

    Revert "Add UltriFire managed gateway policies"

--- ultrifire/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class UltrifireConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ultrifire'
-    verbose_name = 'UltriFire'
--- vulnsight/urls.py
+++ vulnsight/urls.py
@@ -21,6 +21,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('api/', include(router.urls)),
     path('api/dashboard/summary/', DashboardSummaryView.as_view(), name='dashboard-summary'),
-    path('ultrifire/', include('ultrifire.urls')),
     path('', include('dashboard.urls')),
 ]
